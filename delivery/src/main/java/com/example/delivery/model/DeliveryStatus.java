58884215b6057ff0cce6f55c43d00127aa7855b5
sy0419 <psy041912@gmail.com>
2025-09-17 18:34:22 +0900

    Add DeliveryStatus enum.

---
+++ delivery/src/main/java/com/example/delivery/model/DeliveryStatus.java
@@ -0,0 +1,10 @@
+// src/main/java/com/example/delivery/model/DeliveryStatus.java
+
+package com.example.delivery.model;
+
+public enum DeliveryStatus {
+    PREPARING,
+    SHIPPED,
+    IN_TRANSIT,
+    DELIVERED
+}
