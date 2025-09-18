6471e71ea82fe81760ebfc2349243eb7d562e141
sy0419 <psy041912@gmail.com>
2025-09-18 13:34:37 +0900

    Add Delivery repository for database access.

--- delivery/src/main/java/com/example/delivery/repository/DeliveryRepository.java
+++ delivery/src/main/java/com/example/delivery/repository/DeliveryRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.delivery.model.Delivery;
 
 public interface DeliveryRepository extends JpaRepository<Delivery, String> {
-    // invoiceNumber가 String이므로 기본 키 타입은 String
+    
 }
