f29d4ce902ec98487540766978451264da87c542
sy0419 <psy041912@gmail.com>
2025-09-30 19:29:50 +0900

    .

--- delivery/src/main/java/com/example/delivery/exception/DeliveryNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.delivery.exception;
-
-public class DeliveryNotFoundException extends RuntimeException {
-    public DeliveryNotFoundException(String invoiceNumber) {
-        super("Delivery not found for invoice number: " + invoiceNumber);
-    }
-}
--- delivery/src/main/java/com/example/delivery/repository/DeliveryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.delivery.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.delivery.model.Delivery;
-
-public interface DeliveryRepository extends JpaRepository<Delivery, String> {
-    
-}
